952ab5bedca53d400847a910338ebe84045c3063
xelilovkamran <xlilovkamran@gmail.com>
2023-12-03 01:42:08 +0400

    changed prompt for system in openai package

--- app.js
+++ app.js
@@ -17,14 +17,14 @@ async function main(message) {
       {
         role: "system",
         content:
-          "user will give type of exponent and his adress. you have to give museum name and adress which is near to him. Give all answer in Azerbaijani language. if user gives question which is out of scope, you have to give answer like 'i dont know'",
+          "you will be used as a chatbot in web-site. User may input type of historical exponents (optional) which he wants to see inside his text. You will detect museums or some other places where person can see exponetent related to type of inputed exponent. if user input his current place, you will filter these places acoording to user's current adress. if user does not input his current adress, take adress as center of Baku city. you have to give some response as name of places and their addresses and also give information that in the list form (one under another), person how much time spend to go there by using these instructions. If you can't find any place, you have to give some response as 'I can't find any place related to your request'. If question is not related to your task, you have to provide some response as 'I am not using for these purposes'.",
       },
       {
         role: "user",
         content: message,
       },
     ],
-    model: "gpt-3.5-turbo",
+    model: "gpt-4",
   });
 
   return completion.choices[0];
